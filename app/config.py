c1dc1c3cd0f83a8bd3009222bda7d16892401eb1
charchit <charchit.dahiya@gmail.com>
2021-08-12 13:00:36 +0530

    uro

--- app/config.py
+++ app/config.py
@@ -5,10 +5,13 @@ load_dotenv()
 
 class Config:
 	SECRET_KEY = os.getenv('SECRET_KEY')
-	uri = os.getenv("DATABASE_URL")  # or other relevant config var
-	if uri.startswith("postgres://"):
-		uri = uri.replace("postgres://", "postgresql://", 1)
-	SQLALCHEMY_DATABASE_URI = uri
+    uri = os.environ.get("DATABASE_URL") or "sqlite:///test.db"
+    try:
+        if uri.startswith("postgres://"):
+            uri = uri.replace("postgres://", "postgresql://", 1)
+    except Exception as e:
+        print(e)
+    SQLALCHEMY_DATABASE_URI = uri
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	DEBUG = True
 	MAIL_SERVER = "smtp.gmail.com"
