ea29af8b5d5efff4c159b7baa12396567d2b8f89
charchit <charchit.dahiya@gmail.com>
2021-08-12 12:10:14 +0530

    used app.run

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
-from app import create_app,socketio
+from app import create_app
 
 app = create_app()
 
 
 if __name__ == '__main__':
-	port = int(os.environ.get("PORT", 5000))
-	socketio.run(app,host="0.0.0.0",port=port)
+	app.run()
