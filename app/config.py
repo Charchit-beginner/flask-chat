e71f0f47ed3f5cd53ff2a3dd6ac65c909388a764
charchit <charchit.dahiya@gmail.com>
2021-08-12 12:52:07 +0530

    using port

--- app/config.py
+++ app/config.py
@@ -5,7 +5,10 @@ load_dotenv()
 
 class Config:
 	SECRET_KEY = os.getenv('SECRET_KEY')
-	SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+	uri = os.getenv("DATABASE_URL")  # or other relevant config var
+	if uri.startswith("postgres://"):
+    	uri = uri.replace("postgres://", "postgresql://", 1)
+	SQLALCHEMY_DATABASE_URI = uri
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	DEBUG = True
 	MAIL_SERVER = "smtp.gmail.com"
